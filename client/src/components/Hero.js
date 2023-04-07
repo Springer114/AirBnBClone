cc8af1462b860e30781e46428b2858470bb0e055
Zackary Springer <springerzack04@yahoo.com>
2023-04-07 10:21:15 -0700

    created hero component and added styles

---
+++ client/src/components/Hero.js
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="groupPhoto.png" alt="AirBnB grid"/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activites led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
