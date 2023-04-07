be15d9fc814b4a2f4f90b5540142a11d98814bb8
Zackary Springer <springerzack04@yahoo.com>
2023-04-07 10:58:45 -0700

    created card component and styled it, as well as moved photos into images folder

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero() {
     return (
         <section className="hero">
-            <img src="groupPhoto.png" alt="AirBnB grid"/>
+            <img src="images/groupPhoto.png" alt="AirBnB grid"/>
             <h1>Online Experiences</h1>
             <p>Join unique interactive activites led by one-of-a-kind hosts—all without leaving home.</p>
         </section>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav>
-            <img src="airBnB.png" alt="AirBnB logo"/>
+            <img src="images/airBnB.png" alt="AirBnB logo"/>
         </nav>
     )
 }
